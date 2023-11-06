bb6be828ec84496820c48881de735daf5a247700
Emiliano Fernandez <emilianofh02@gmail.com>
2023-11-06 01:09:02 -0700

    Validaciones de rutas

--- routes/web.php
+++ routes/web.php
@@ -17,42 +17,59 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/suma/{numeros}', function ($numeros) {  
+Route::pattern('numero', '[0-9]+');
+Route::get('/suma/{numeros}', function ($numeros) {
     $numerosArray = explode('/', $numeros);
     return array_sum($numerosArray);
-}) -> where('numeros', '.*');
+})->where('numeros', '(\d+\/)+\d+');
 
-Route::get('/resta/{numeros}', function ($numeros) {  
+Route::get('/resta/{numeros}', function ($numeros) {
     $numerosArray = explode('/', $numeros);
     $resultado = null;
 
-    foreach($numerosArray as $numero) {
-        if($resultado === null) {
+    foreach ($numerosArray as $numero) {
+        if ($resultado === null) {
             $resultado = $numero;
         } else {
             $resultado = $resultado - $numero;
         }
     }
     return $resultado;
-}) -> where('numeros', '.*');
+})->where('numeros', '(\d+\/)+\d+');
 
-Route::get('/division/{numeros}', function ($numeros) {  
+Route::get('/division/{numeros}', function ($numeros) {
     $numerosArray = explode('/', $numeros);
-    return array_reduce($numerosArray, function($value, $num) {
-        if($value === null) {
+    return array_reduce($numerosArray, function ($value, $num) {
+        if ($value === null) {
             return $num;
         }
         return $value / $num;
     }, null);
-}) -> where('numeros', '.*');
+})->where('numeros', '(\d+\/)+\d+');
 
-Route::get('/saludo/{nombre}/{apellidos?}', function ($nombre, $apellidos = "Doe") {  
-    return 'Hola '. $nombre .' '. $apellidos;
-});
+Route::get('/multiplicacion/{numeros}', function ($numeros) {
+    $numerosArray = explode('/', $numeros);
+    return array_reduce($numerosArray, function ($value, $num) {
+        if ($value === null) {
+            return $num;
+        }
+        return $value * $num;
+    }, null);
+})->where('numeros', '(\d+\/)+\d+');
 
-Route::get('/greeting/{name?}', function ($name = 'John') {  
-    return view('example', ['name'=> $name]);
-});
+
+Route::get('/saludo/{nombre}/{apellidos?}', function ($nombre, $apellidos = "Doe") {
+    return 'Hola ' . $nombre . ' ' . $apellidos;
+})->where([
+            'nombre' => '[A-Za-z]+',
+            'apellidos' => '[A-Za-z]+'
+        ]);
+
+Route::get('/greeting/{name?}', function ($name = 'John') {
+    return view('example', ['name' => $name]);
+})->where([
+            'name' => '[A-Za-z]+',
+        ]);
 
 
 
